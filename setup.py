cc75fe552d99bd1f903f1b316b64be2fb32f4b4e
Mika Lackman <mika.lackman@gmail.com>
2015-12-08 19:10:22 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,30 +1,22 @@
-import collections
-import inspect
-import json
+from distutils.core import setup
 
-
-class Mapper(object):
-
-  def __init__(self):
-    if not hasattr(self, '__target_class__'):
-      raise Exception("Define __target_class__")
-    self.__mappings__ = collections.defaultdict(lambda: None)
-    ignored_attributes = ['to_hash', 'from_hash']
-    for attr in dir(self):
-      if attr not in ignored_attributes and not attr.startswith('__') and not attr.endswith('__'):
-        self.__mappings__[attr] = getattr(self, attr)
-
-  def to_hash(self, obj):
-    result = dict()
-    for store_key, attribute in self.__mappings__.items():
-      value = attribute.get(obj)
-      result[store_key] = value
-    return result
-
-  def from_hash(self, hash):
-    obj = self.__target_class__.__new__(self.__target_class__)
-    for store_key, attribute in self.__mappings__.items():
-      data = hash[store_key]
-      attribute.set(obj, data)
-    return obj
-
+setup(
+  name = "pymapp",
+  packages = ['pymapp'],
+  version = "0.01",
+  description = "Python object from hash and to hash mapper",
+  author = "Mika Lackman",
+  author_email = "mika(at)gmail.com",
+  #url = "https://github.com/mlackman/pyavali/tree/master/pyavali",
+  keywords = ["mapper"],
+  classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Development Status :: 4 - Alpha",
+    "Environment :: Other Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache License 2.0",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
